9c304f7b16f03eaa19e4c2406707e43fb4934ad4
whalecake <ingrid.ang@codenation.org>
2019-10-16 22:09:34 +0000

    made a bunch of student instructions

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
 $( "document" ).ready(function() {
-
-
-
+$("#button").click(function(){
+        //Step One: select the user input so that in can be used in...
+        var userInput=$(".input").val();
+        //...Step Two: the userInput is the parameter (input) for our pigLatinizer function!
+        var pigLatinWord=pigLatinizer(userInput);
+        //Step Three: display the output of our pigLatinizer function!
+        $(".output").text(pigLatinWord);
 });
+
+//We need to define the pigLatinizer function and write the code to make it work!
+function pigLatinizer (userInput) {
+    //Think about: if "banana" goes in, what comes out?
+    //Think about: if "elephant" goes in, what comes out?
+    };
+
+
+
+
